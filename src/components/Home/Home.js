13410957862613375a45a31b2a790020808c6e3e
chen.dong <dong.chen@rea-group.com>
2017-05-08 22:08:22 +0800

    new component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { container, title, slogan } from './styles.css';
+
+const Home = () => (
+  <div className={container}>
+    <p className={title}>Duckr</p>
+    <p className={slogan}>The Real Time, Cloud Based, modular platform.</p>
+  </div>
+  );
+
+export default Home;
---
+++ src/containers/Authenticate/AuthContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Authenticate } from '../../components';
+
+const AuthContainer = () => (
+  <Home />
+);
+
+export default AuthContainer;
